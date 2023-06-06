a000b996dfaa64e44ee2930175b192a7d177fee1
heviahxc <heviamc@gmail.com>
2023-06-06 10:39:42 -0400

    Link

---
+++ src/routes/pokemons/list-ssr/index.tsx
@@ -0,0 +1,16 @@
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  return <>Hola Mundo</>
+});
+
+export const head: DocumentHead = {
+  title: 'List - SSR',
+  meta: [
+    {
+      name: 'description',
+      content: 'Qwik site description',
+    },
+  ],
+};
